c02acdef29d0fcad80f5a2879fb99aa996acfffa
Glenn Spinner <glenngineer1@gmail.com>
2016-09-16 14:21:58 -0500

    have info from database returning to home page

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,13 @@ const router = Router()
 const newPost = require('../models/newPost')
 
 router.get('/', (req, res) => {
-  console.log(newPost.find({ title: /love/ }, { title: 1 })
-  .then((data) => console.log(data)))
-  res.render('index')
+  newPost
+  .find()
+  .then((returnedData) => {
+  // const returnedData = newPost.find({ title: / /}, {"title": "/ /", "link": "/ /", "image": "/ /", _id: 0 })
+  // .then((returnedData) => console.log("returned data from above: ", returnedData))
+    res.render('index', {returnedData})
+  })
 })
 
 router.get('/newPost', (req, res) =>
